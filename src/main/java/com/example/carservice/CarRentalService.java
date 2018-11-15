9382cc9703600012a62c33424812be5f856dbb59
alexander <alexchueshev@gmail.com>
2018-11-15 17:22:37 +0100

    optimize imports

--- src/main/java/com/example/carservice/CarRentalService.java
+++ src/main/java/com/example/carservice/CarRentalService.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @RestController
 public class CarRentalService {
